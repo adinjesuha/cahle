c86eb0bf049410a2736e5ee002310da51db2a9d8
Adin Jesuha <adinjesuha@gmail.com>
2019-03-25 07:54:23 -0600

    add two new posts

---
+++ src/pages/informacion-practica.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import Layout from '../components/layout'
+import Container from '../components/container'
+import Objetivos from '../images/objetivos.jpg'
+import HeroPage from '../components/heroPage' 
+
+export default () => {
+  return (
+    <Layout>
+      <HeroPage title="Historia, Misión, Visión y Objetivos" tags="acerca de" bgImage={Objetivos}/>
+      <Container>
+
+      </Container>
+    </Layout>
+  )
+}
