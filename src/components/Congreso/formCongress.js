f3114d5695334298da6e54e4ed3d1598e40449b6
Adin Castro <acastro@agrobiotek.com>
2020-02-16 19:10:59 -0600

    add congress form

---
+++ src/components/Congreso/formCongress.js
@@ -0,0 +1,200 @@
+import React from 'react'
+import { navigate } from 'gatsby-link'
+
+import { encode } from '../../utils/encode'
+import styled from 'styled-components'
+
+const Form = styled.form`
+  padding: 40px 0;
+`
+
+const InputRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
+const StyledInputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 48%;
+  &.full-width{
+    width: 100% !important;
+  }
+  label{
+    margin-bottom: 10px;
+  }
+`
+
+const StyledButton = styled.button`
+  background-color: var(--oxford-blue);
+  display: inline-block;
+  width: 100%;
+  padding: 10px 0 !important;
+  font-size: 18px;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+`
+
+const Input = ({handleChange, ...restProps}) => {
+  return (
+    <input
+      onChange={handleChange}
+      {...restProps}
+    />
+  )
+}
+
+const Select = ({handleChange, ...restProps}) => (
+  <select 
+    onChange={handleChange} 
+    {...restProps}
+  >
+    <option value="default">Seleccionar</option>
+    {restProps.options.map((option, index) => {
+      return(
+        <option key={index} value={option}>{option}</option>
+      )
+    })}
+  </select>
+)
+
+const InputBlock = ({label, handleChange, classNames, isSelect, ...restProps}) => (
+  <StyledInputWrapper className={classNames}>
+    <label>{label}</label>
+    {!isSelect ?
+      <Input
+        handleChange={handleChange}
+        {...restProps}
+      /> :
+      <Select 
+        handleChange={handleChange} 
+        {...restProps}
+      />
+    }
+  </StyledInputWrapper>
+)
+
+
+class SubscribeForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    const form = e.target
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...this.state,
+      }),
+    })
+    .then(() => navigate(form.getAttribute('action')))
+    .catch(error => alert(error))
+  }
+
+  render() {
+    return (
+      <Form
+        name="congreso"
+        method="post"
+        action="/thanks/"
+        data-netlify="true"
+        ata-netlify-honeypot="bot-field"
+        data-netlify-recaptcha="true"
+        onSubmit={this.handleSubmit}
+      >
+      <div className="field-wrapper">
+        <input type="hidden" name="form-name" value="congreso" />
+        <p hidden>
+          <label>
+            Don’t fill this out:{' '}
+            <input name="bot-field" onChange={this.handleChange} />
+          </label>
+        </p>
+        <InputRow>
+          <InputBlock 
+            type="text"
+            name="nombre"
+            label="Nombre *"
+            handleChange={this.handleChange}
+            required
+          />
+          <InputBlock 
+            type="text"
+            name="apellido"
+            label="Apellido *"
+            handleChange={this.handleChange}
+            required
+          />
+        </InputRow>
+        <InputRow>
+          <InputBlock 
+            type="text"
+            name="genero"
+            label="Genero"
+            isSelect={true}
+            options={["hombre", "mujer"]}
+            handleChange={this.handleChange}
+          />
+          <InputBlock 
+            type="text"
+            name="identificacion"
+            label="Identificacion *"
+            handleChange={this.handleChange}
+            required
+          />
+        </InputRow>
+        <InputBlock 
+          type="email"
+          name="correo"
+          label="Correo *"
+          handleChange={this.handleChange}
+          classNames="full-width"
+          required
+        />
+        <InputRow>
+          <InputBlock 
+            type="text"
+            name="pais"
+            label="País de residencia"
+            handleChange={this.handleChange}
+          />
+          <InputBlock 
+            type="text"
+            name="cuidad"
+            label="Ciudad de residencia"
+            handleChange={this.handleChange}
+          />
+        </InputRow>
+        <InputRow>
+          <InputBlock 
+            type="tel"
+            name="telefono"
+            label="Teléfono"
+            handleChange={this.handleChange}
+          />
+          <InputBlock 
+            type="text"
+            name="ocupación"
+            label="Ocupación"
+            isSelect={true}
+            options={["Estudiante extranjero", "Estudiante nacional", "Ganadero (productor de leche)", "Ganadero (procesador)", "Profesional nacional", "Profesional extranjero", "Investigador", "Otro" ]}
+            handleChange={this.handleChange}
+          />
+        </InputRow>
+      </div>
+      <StyledButton type="submit" className="button-btn">Enviar</StyledButton>
+      <p><strong>Importante: </strong>Sólo se aprueban las inscripciones de las personas que envíen el comprobante de pago escaneado al correo electrónico: <a href="mailto:admoncahle@gmail.com">admoncahle@gmail.com</a></p>
+    </Form>
+  )}
+}
+
+export default SubscribeForm
--- src/pages/inscripciones-congreso-2020.js
+++ src/pages/inscripciones-congreso-2020.js
@@ -1,161 +1,51 @@
 import React from 'react'
-import { navigate } from 'gatsby-link'
 import styled from 'styled-components'
 
 import Layout from '../components/layout'
 import Container from '../components/container'
+import SubscribeForm from '../components/Congreso/formCongress'
 import { device } from '../styles/breakpoints'
-import { encode } from '../utils/encode'
 
+const Wrapper = styled.div`
+  max-width: 680px;
+  padding: 30px 0;
+  margin: 0 auto;
+`
 
-class SubscribeForm extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
+const HeroSection = styled.div`
+  width: 100%;
+  min-height: 220px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  h2{
+    color: var(--oxford-blue);
+    font-size: 32px;
+    text-transform: uppercase;
+    margin-top: 80px;
   }
-
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value })
+  @media ${device.tablet}{
+    height: 40vh;
+    h2{
+      font-size: 38px;
+    }
   }
-
-  handleSubmit = e => {
-    e.preventDefault()
-    const form = e.target
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({
-        'form-name': form.getAttribute('name'),
-        ...this.state,
-      }),
-    })
-    .then(() => navigate(form.getAttribute('action')))
-    .catch(error => alert(error))
-  }
-  render() {
-    return (
-      <form
-          name="congreso"
-          method="post"
-          action="/thanks/"
-          data-netlify="true"
-          ata-netlify-honeypot="bot-field"
-          data-netlify-recaptcha="true"
-          onSubmit={this.handleSubmit}
-        >
-          <div className="field-wrapper">
-            {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-        <input type="hidden" name="form-name" value="congreso" />
-        <p hidden>
-          <label>
-          Don’t fill this out:{' '}
-          <input name="bot-field" onChange={this.handleChange} />
-        </label>
-      </p>
-      <p>
-        <label>Nombre Completo:</label>
-        <br />
-        <input
-          className="relative-field"
-          type="text"
-          name="Nombre"
-          onChange={this.handleChange}
-        />
-        </p>
-      <p className="select-group">
-        <label>Genero:</label>
-        <br />
-        <select
-          className="select"
-        >
-          <option value="default">Seleccionar</option>
-          <option value="hombre">Hombre</option>
-          <option value="mujer">Mujer</option>
-        </select>
-      </p>
-      <p>
-      <label>Correo electrónico:</label>
-      <input
-        className="relative-field"
-        type="email"
-        name="Correo"
-        onChange={this.handleChange}
-      />
-    </p>
-    <p>
-      <label>País de residencia:</label>
-      <input
-        className="relative-field"
-        type="text"
-        name="País"
-        onChange={this.handleChange}
-      />
-    </p>
-    <p>
-      <label>Ciudad de residencia:</label>
-      <input
-        className="relative-field"
-        type="text"
-        name="Ciudad"
-        onChange={this.handleChange}
-      />
-    </p>
-    <p>
-      <label>Teléfono:</label>
-      <input
-        className="relative-field"
-        type="tel"
-        name="Teléfono"
-        onChange={this.handleChange}
-      />
-    </p>
-    <p>
-      <label>Institución u organización afiliado:</label>
-      <input
-        className="relative-field"
-        type="text"
-        name="Afiliado"
-        onChange={this.handleChange}
-      />
-    </p>
-    <p className="select-group">
-      <label>Ocupación:</label>
-      <select
-        className="select"
-      >
-        <option value="default">Seleccionar</option>
-        <option value="Estudiane nacional">Estudiante nacional</option>
-        <option value="Estudiante extranjero">Estudiante extranjero</option>
-        <option value="Ganadero">Ganadero</option>
-        <option value="Profesional nacional">Profesional nacional</option>
-        <option value="Profesional extranjero">Profesional extranjero</option>
-        <option value="Investigador">Investigador</option>
-        <option value="Otro">Otro</option>
-      </select>
-    </p>
-    <p className="select-group">
-      <label>Tipo de participación:</label>
-      <select className="select">
-        <option value="default">Seleccionar</option>
-        <option value="Asistente">Asistente</option>
-        <option value="Ponente">Ponente (oral o póster)</option>
-        <option value="Conferencista magistral">Conferencista magistral</option>
-      </select>
-    </p>
-  </div>
-  <button type="submit" className="button-btn button-position">Enviar</button>
-  </form>
-  )
-  }
-}
-
+`
 
 
 const CongresoInscripciones = () => {
   return (
     <Layout>
       <Container>
-        <SubscribeForm />
+        <Wrapper>
+          <HeroSection>
+            <h2>Inscripciones del Congreso</h2>
+          </HeroSection>
+          <p className="main-text navy">Utilice el formulario adjunto para solicitar su registro al VIII Congreso Nacional de la Leche</p>
+          <SubscribeForm />
+        </Wrapper>
       </Container>
     </Layout>
   )
