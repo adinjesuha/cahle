9eb9abce4f4d361733086b4f3ac2d23501843e23
Adin Castro <acastro@agrobiotek.com>
2020-02-17 10:37:27 -0600

    remove google-maps-react

--- src/components/Congreso/practicalinfo.js
+++ src/components/Congreso/practicalinfo.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-import GMap from '../GMap'
 import { Wrapper } from './styles'
 import SubscriptionLink from './SubscriptionLink'
+import AgafamMap from '../../images/agafam.jpg'
 
 const Practicalinfo = () => (
   <div>
@@ -12,13 +12,9 @@ const Practicalinfo = () => (
       <p className="main-text navy">El VIII Congreso Nacional de la Leche tendrá lugar en las instalaciones de la Asociación de Ganaderos y Agricultores de Francisco Morazán (AGAFAM) en la ciudad de Tegucigalpa del 14 al 16 de mayo del 2020 con la participación destacada de diferentes empresas y academia. Además, contará con transmisión en vivo de las conferencias.</p>
       <h3>¿Cómo Llegar?</h3>
       <p className="main-text navy">Predio ferial de AGAFAM, Tegucigalpa, Fco. Morazán.</p>
-      <GMap 
-        center= {{
-          lat: 14.0738976,
-          lng: -87.1585019
-        }}
-        zoom={16}
-      />
+      <a href="https://goo.gl/maps/1CF5aAzJPrFZgRho6" target="_blank" rel="noopener noreferrer">
+        <img src={AgafamMap} alt="Agafam Congreso de la leche 2020"/>
+      </a>
       <p className="main-text navy">Es el principal predio ferial de la ciudad para la realización de exposiciones, ferias y demás eventos, con una ubicación privilegiada en el centro del país.</p>
     </Wrapper>
     <SubscriptionLink />
