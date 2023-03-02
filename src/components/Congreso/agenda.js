a4e589342834129c6131ec2539d67b09fe8b9feb
adinjesuhals <adinjesuha@gmail.com>
2023-03-02 12:02:15 -0600

    Update Agenda image

--- src/components/Congreso/agenda.js
+++ src/components/Congreso/agenda.js
@@ -3,14 +3,12 @@ import React from 'react'
 import { Wrapper } from './styles'
 import SubscriptionLink from './SubscriptionLink'
 
-import Cuadrifolio1 from '../../images/cuadrifolio-01.png'
-import Cuadrifolio2 from '../../images/cuadrifolio-02.png'
+import AgendaPreliminar from '../../images/agenda-preliminar.png'
 
 const Agenda = () => (
   <div>
     <Wrapper>
-      <img src={Cuadrifolio1} alt="Agafam Congreso de la leche 2020"/>
-      <img src={Cuadrifolio2} alt="Agafam Congreso de la leche 2020"/>
+      <img src={AgendaPreliminar} alt="Agafam Congreso de la leche 2020"/>
     </Wrapper>
     <SubscriptionLink />
   </div>
