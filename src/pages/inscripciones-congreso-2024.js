09e8b608f52ab81625ff5a18249a484e3fcfc406
adinjesuhals <adinjesuha@gmail.com>
2024-04-09 14:15:27 -0600

    change estudiantes cuota

--- src/pages/inscripciones-congreso-2024.js
+++ src/pages/inscripciones-congreso-2024.js
@@ -82,10 +82,10 @@ const CongresoInscripciones = () => {
                       <p>* Estudiantes (L.)</p>
                     </div>
                     <div className="cell" data-title="Hasta el 31 de marzo">
-                      <p>500.00</p>
+                      <p>600.00</p>
                     </div>
                     <div className="cell" data-title="Del 01 de abril al 02 de mayo">
-                      <p>600.00</p>
+                      <p>700.00</p>
                     </div>
                   </div> 
                   <div className="c-row">
@@ -93,10 +93,10 @@ const CongresoInscripciones = () => {
                       <p>Asociados CAFOGAH y CAHLE (L.)</p>
                     </div>
                     <div className="cell" data-title="Hasta el 31 de marzo">
-                      <p>600.00</p>
+                      <p>700.00</p>
                     </div>
                     <div className="cell" data-title="Del 01 de abril al 02 de mayo">
-                      <p>700.00</p>
+                      <p>800.00</p>
                     </div>
                   </div> 
                   <div className="c-row">
